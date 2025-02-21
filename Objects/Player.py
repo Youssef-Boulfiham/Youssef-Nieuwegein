26cfd5bc668a561b653489bc2d835778f9001de4
youssefboulfiham <youssef.boulfiham@student.hu.nl>
2025-02-21 13:55:15 +0100

    Code clean

--- Objects/Player.py
+++ Objects/Player.py
@@ -11,7 +11,7 @@ from Objects.Pathfinding import AStar
 class Player:
 
     def __init__(self, position_start):
-        self.df = pd.read_csv('/Users/youssefboulfiham/PycharmProjects/pythonProject/Youssef-Nieuwegein/Objects/df_player.csv', sep=';')
+        self.df = pd.read_csv('/Users/youssefboulfiham/PycharmProjects/pythonProject/Youssef-Nieuwegein/Objects/df_player.csv', sep=';', dtype=float)
         self.path = []
         self.position_current = position_start
         self.activity_current = "school"
@@ -37,50 +37,20 @@ class Player:
         self.path.pop(0)
 
     def set_activity_next(self):
-        data = {
-            "ervaren gezondheid": [80],
-            "leeftijd": [0.53],
-            "geslacht": [0.54],
-            "roken": [0.03],
-            "alcohol": [0.04],
-            "softdrugs": [0.02],
-            "harddrugs": [0.01],
-            "thuis": [0.3],
-            "school": [0.4],
-            "vrienden": [0.1],
-            "vrije tijd": [0.2]
-        }
-        df = pd.DataFrame(data)
-
-        # Extract activities and their probabilities
-        activities = df.iloc[0, 7:9].to_dict()  # Select only activity columns
+        activities = self.df.iloc[0, 7:9].to_dict()
         activity_names = list(activities.keys())
         activity_probs = np.array(list(activities.values()))
-
-        # Normalize probabilities to sum to 1
         activity_probs /= activity_probs.sum()
-
-        # Compute cumulative distribution function (CDF)
         cumsum_activities = np.cumsum(activity_probs)
-
-        # Randomly select an activity
-        random_number = np.random.rand()  # Random number in [0, 1)
-
-        # Find the first index where random_number is less than the CDF
+        random_number = np.random.rand()
         chosen_index = np.searchsorted(cumsum_activities, random_number)
-
-        # Output the selected activity
         activity_next = activity_names[chosen_index]
-        print(f"Selected activity: {activity_next}")
-
         self.path += self.Pathfinding.search_path(start=self.position_current,
                                                   goal=self.activities["weg_hart"],
                                                   allowed_colors=[self.activities_[self.activity_current], "black"])
         self.path += self.Pathfinding.search_path(start=self.activities["weg_hart"],
                                                   goal=random.choice(self.activities[activity_next]),
                                                   allowed_colors=["black", self.activities_[activity_next]])
-        print(self.activity_current, activity_next)
-        print(self.position_current, len(self.path))
         self.activity_current = activity_next
 
     def step_idle(self):
