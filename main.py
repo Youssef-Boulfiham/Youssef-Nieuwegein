4482e6eeab38f800b88ca3bf3b585a329c16a87a
youssefboulfiham <youssef.boulfiham@student.hu.nl>
2025-03-25 11:19:04 +0100

    implement substance_use()

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ matplotlib.use('TkAgg')
 if __name__ == "__main__":
     date_start = datetime(2024, 1, 1)
     date_end = datetime(2026, 1, 1)
-    steps_max = 2000 * 365 * 2  # stappen * dagen * jaren
+    steps_max = 2000 * 365 * 2  # stappen * dagen * jarena
     agents_count = 24
     game = Env(agents_count, date_start, date_end, steps_max)
